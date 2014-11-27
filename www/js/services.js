b36950fdb8c6cb6eb55b314b0e780b42e59a9315
ihoover <polonian@gmail.com>
2014-11-27 05:57:58 -0500

    manual override  option

--- www/js/services.js
+++ www/js/services.js
@@ -1,29 +1,48 @@
-angular.module('starter.services', [])
+function temp_to_string(temp){
+  return String(temp) + "\u2109";
+}
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+function hum_to_string(humidity){
+  return String(humidity) + '%';
+}
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+function identity(value){
+  return value;
+}
 
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
+function prepare(factor){
+  var states = ['off', 'on'];
+  var prepared = {
+            id: factor.id,
+            states: states,
+            state: factor.state,
+            name: factor.name,
+            overview: factor.string(factor.value.slice(-1)[0]),
+            value: factor.value,
+            device: factor.device
+          };
+  return prepared
+}
+
+function override(){
+    var state_word = document.getElementById("current-state-word");
+    var current_state = state_word.innerHTML;
+    var not_state_word = document.getElementById("current-not-state-word");
+    state_word.innerHTML = not_state_word.innerHTML;
+    not_state_word.innerHTML = current_state;
+    
+    var warning = document.getElementById("override-warning");
+    if (warning.style.visibility == 'visible')
+    {
+      warning.style.visibility = 'hidden';
     }
-  }
-})
+    else
+    {
+      warning.style.visibility = 'visible';
+    }
+}
+
+angular.module('starter.services', [])
 
 /**
  * A service that returns the current environmental factors being measured and 
@@ -31,22 +50,45 @@ angular.module('starter.services', [])
  */
  
 .factory('Factors', function() {
-  
   var factors = [
-    { id: 0, name: "Temperature", value: [58,58,57,56,58,57,59,60,61,62,63,63,62,62]},
-    { id: 1, name: "Humidity", value: [33, 33,33,32,32,31,31,32,32,33,33,34,34,34,32]},
-    { id: 2, name: "Soil Moisture", value: [73]},
-    { id: 3, name: "Lights", value: ['on']}
+    { id: 0, 
+      name: 'Temperature',
+      state: 1,
+      value: [58,58,57,56,58,57,59,60,61,62,63,63,62,62],
+      device: 'heater',
+      string: temp_to_string},
+    { id: 1,
+      name: 'Humidity',
+      value: [33,33,33,32,32,31,31,32,32,33,33,34,34,34,32],
+      state: 0,
+      device: 'fan',
+      string: hum_to_string},
+    { id: 2,
+      name: 'Soil Moisture',
+      value: [73],
+      state: 1,
+      device: 'irrigation',
+      string: hum_to_string},
+    { id: 3,
+      name: 'Lights',
+      value: ['on'],
+      state: 1,
+      device: 'lights',
+      string: identity}
   ];
 
-  
   return {
     all: function() {
-      return factors;
+      //return factors;
+      var overview = [];
+      for (var i = 0; i < factors.length; i++){
+        overview.push(prepare(factors[i]));
+      }
+      return overview;
     },
     get: function(factorId) {
       // Simple index lookup
-      return factors[factorId];
+      return prepare(factors[factorId]);
     }
   }
   
